dfab5f5ae4fe7fbf184debea7ea727a23183d71f
Fodan Deng <fodan.deng@gmail.com>
2020-06-16 12:06:51 +0800

    init commit

---
+++ LeetCode2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LeetCode2
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            bool additionalOne = false;
+            ListNode head = new ListNode(-1);
+            ListNode current = new ListNode(-1);
+            head.next = current;
+            do
+            {
+                int v1 = 0; int v2 = 0;
+                if (l1 != null) { v1 = l1.val; l1 = l1.next; }
+                if (l2 != null) { v2 = l2.val; l2 = l2.next; }
+                int num = additionalOne ? v1 + v2 + 1 : v1 + v2;
+                additionalOne = false;
+                if (num > 9) { num -= 10; additionalOne = true; }
+                current.next = new ListNode(num);
+                current = current.next;
+            } while (l1 != null || l2 != null || additionalOne);
+            return head.next.next;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            ListNode l = s.AddTwoNumbers(new ListNode(5), new ListNode(5));
+            do
+            {
+                Console.WriteLine(l.val);
+                l = l.next;
+            } while (l != null);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LeetCode297/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3) { left = new TreeNode(4) { left = new TreeNode(6) }, right = new TreeNode(5) };
+            Codec c = new Codec();
+            Console.WriteLine( c.serialize(root));
+
+            root = c.deserialize(c.serialize(root));
+            Console.WriteLine(root.ToString());
+        }
+    }
+
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+        public override string ToString()
+        {
+            return $"TN {val}: left {(left == null ? "null": left.val.ToString())}, right {(right == null ? "null" : right.val.ToString())}";
+        }
+    }
+    public class Codec
+    {
+
+        // Encodes a tree to a single string.
+        public string serialize(TreeNode root)
+        {
+            List<string> nodeVals = new List<string>();
+            if (root == null) { return "null"; }
+            List<TreeNode> currentLayer = new List<TreeNode>() { root };
+            do
+            {
+                currentLayer = ProceedNextLayer(ref currentLayer, ref nodeVals);
+            } while (currentLayer != null);
+            return string.Join(",", nodeVals);
+        }
+
+        public List<TreeNode> ProceedNextLayer(ref List<TreeNode> currentLayer, ref List<string> nodeVals)
+        {
+            Console.WriteLine($"Current Layer:");
+            int nullCount = 0;
+            const string NULL = "null";
+            List<TreeNode> nextLayer = new List<TreeNode>();
+            foreach (TreeNode tn in currentLayer)
+            {
+                if (tn == null)
+                {
+                    Console.WriteLine("TN: null");
+                    nodeVals.Add(NULL);
+                }
+                else
+                {
+                    Console.WriteLine(tn.ToString());
+                    nodeVals.Add(tn.val.ToString());
+                    if (tn.left == null) { ++nullCount; }
+                    if (tn.right == null) { ++nullCount; }
+                    nextLayer.Add(tn.left);
+                    nextLayer.Add(tn.right);
+                }
+            }
+            return nullCount == nextLayer.Count ? null : nextLayer;
+        }
+
+
+        public TreeNode deserialize(string data)
+        {
+            string[] nodeVals = data.Split(',');
+            if (nodeVals[0] == "null") { return null; }
+
+            int vInd = 1;
+            TreeNode root = new TreeNode(int.Parse(nodeVals[0]));
+            List<TreeNode> currentLayer = new List<TreeNode>() { root };
+            do
+            {
+                currentLayer = ParseNextLayer(ref currentLayer, ref vInd, ref nodeVals);
+                Console.WriteLine(currentLayer.Count.ToString());
+            } while (currentLayer.Count > 0);
+
+            return root;
+        }
+
+        public List<TreeNode> ParseNextLayer(ref List<TreeNode> currentLayer, ref int vInd, ref string[] nodeVals)
+        {
+            List<TreeNode> nextLayer = new List<TreeNode>();
+            foreach(TreeNode tn in currentLayer)
+            {
+                if (tn != null)
+                {
+                    try
+                    {
+                        string l = nodeVals[vInd++];
+                        string r = nodeVals[vInd++];
+                        if (l != "null")
+                        {
+                            TreeNode tnode = new TreeNode(int.Parse(l));
+                            nextLayer.Add(tnode);
+                            tn.left = tnode;
+                        }
+                        if (r != "null")
+                        {
+                            TreeNode tnode = new TreeNode(int.Parse(r));
+                            nextLayer.Add(tnode);
+                            tn.right = tnode;
+                        }
+                    }
+                    catch
+                    {
+                        return nextLayer;
+                    }
+                }
+            }
+            return nextLayer;
+        }
+    }
+
+}
