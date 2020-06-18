c683da5cd14fb8fa18a87882620b0e599ee0494d
Fodan Deng <fodan.deng@gmail.com>
2020-06-18 11:12:25 +0800

    Update LC1014 and LC1028

---
+++ LeetCode1014/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace LeetCode1014
+{
+    class Program
+    {
+        public class Solution
+        {
+            public int MaxScoreSightseeingPair(int[] A)
+            {
+                int l = A[0];
+                int res = int.MinValue;
+
+                for (int ind = 1; ind < A.Length; ++ind)
+                {
+                    int sum = l + A[ind] - ind;
+                    res = sum > res ? sum : res;
+                    sum = A[ind] + ind;
+                    l = sum > l ? sum : l;
+                }
+                return res;
+            }
+        }
+
+        public class Solution2
+        {
+            struct CS { public int index; public int value; public CS(int ind, int val) { this.index = ind; this.value = val; } }
+            public int MaxScoreSightseeingPair(int[] A)
+            {
+                List<CS> cList = new List<CS>();
+                for (int ind = 0; ind < A.Length; ++ind) { cList.Add(new CS(ind, A[ind])); }
+
+                var tmpQ = from a1 in cList
+                           from a2 in cList
+                           let res1 = a1.index - a2.index + a1.value + a2.value
+                           where a1.index < a2.index
+                           orderby res1 descending
+                           select res1;
+
+                foreach (var tmpVar in tmpQ) { return tmpVar; }
+                return 0;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] A = { 8, 1, 5, 2, 6 };
+            Solution s = new Solution();
+            Solution2 s2 = new Solution2();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int res1 = s.MaxScoreSightseeingPair(A);
+            long et1 = sw.ElapsedMilliseconds;
+            int res2 = s.MaxScoreSightseeingPair(A);
+            long et2 = sw.ElapsedMilliseconds;
+            Console.WriteLine($"Solution 1: {res1}, elapsed time: {et1}ms");
+            Console.WriteLine($"Solution 2: {res2}, elapsed time: {et2 - et1}ms");
+        }
+    }
+}
---
+++ LeetCode1028/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LeetCode1028
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+
+    }
+    public class Solution
+    {
+        public TreeNode RecoverFromPreorder(string S)
+        {
+            int firstDash = 0;
+            while (firstDash < S.Length && S[firstDash] != '-') { firstDash++; }
+            TreeNode root = new TreeNode(int.Parse(S.Substring(0,firstDash).ToString()));
+            int dashCount = 0;
+            StringBuilder sb = new StringBuilder();
+            List<TreeNode> tnList = new List<TreeNode>{ root };
+
+            for (int ind = firstDash; ind < S.Length; ++ind)
+            {
+                if (S[ind] == '-' || ind == S.Length - 1)
+                {
+                    if (sb.Length == 0 && ind != S.Length - 1)
+                    {
+                        dashCount++;
+                    }
+                    else
+                    {
+                        if (ind == S.Length -1) { sb.Append(S[ind]); }
+                        int val = int.Parse(sb.ToString());
+                        sb.Clear();
+
+                        try { tnList[dashCount] = new TreeNode(val); }
+                        catch (ArgumentOutOfRangeException) { tnList.Add(new TreeNode(val)); }
+
+                        if (tnList[dashCount - 1].left == null) 
+                        { tnList[dashCount - 1].left = tnList[dashCount]; }
+                        else 
+                        { tnList[dashCount - 1].right = tnList[dashCount]; }
+
+                        dashCount = 1;
+                    }
+
+                }
+                else
+                {
+                    sb.Append(S[ind]);
+                }
+            }
+            return root;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = "1-2--3--4-5--6--7";
+            Solution s = new Solution();
+            s.RecoverFromPreorder(S);
+        }
+    }
+
+}
