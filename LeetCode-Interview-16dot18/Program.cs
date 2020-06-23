2d23c4225f3b56655f1579c58d80610d25fc72be
Fodan Deng <fodan.deng@gmail.com>
2020-06-23 09:53:02 +0800

    Update Interview 16.18, LC1028

---
+++ LeetCode-Interview-16dot18/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace LeetCode_Interview_16dot18
+{
+    public class Solution
+    {
+        public bool PatternMatching(string pattern, string value)
+        {
+            // la -> length of 'a' string pattern
+            // lb -> length of 'b' string pattern
+            // lp -> length of pattern (total number of chars in pattern)
+            // lv -> length of value (total number of chars in value)
+            // aCount -> number of 'a's in pattern
+            // bCount -> you got it
+
+            int aCount = 0;
+            int bCount = 0;
+
+
+            foreach (char c in pattern)
+            {
+                if (c == 'a')
+                { aCount++; }
+                else
+                { bCount++; }
+            }
+            if (value.Length == 0) { return bCount == 0; }
+            if (pattern.Length == 0) { return false; }
+            // swap a, b if the number of 'b' in pattern is greater than 'a'.
+            if (aCount < bCount)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int ind = 0; ind < pattern.Length; ++ind)
+                { sb.Append(pattern[ind] == 'a' ? 'b' : 'a'); }
+                pattern = sb.ToString();
+                int tmp = 0;
+                aCount = bCount;
+            }
+
+            for (int la = 0; aCount * la <= value.Length; ++la)
+            {
+
+                // la * aCount + (lp - aCount) * lb = lv
+                // ->>  
+                //      lb = (lv - la * aCount) / (lp - aCount)    must be a integer
+
+                double lenB = (double)(value.Length - la * aCount) / (double)(pattern.Length - aCount);
+                if (Math.Abs(lenB - Math.Round(lenB)) > 1e-6)
+                {
+                    continue;
+                }
+                else
+                {
+                    bool res = true;
+                    int position = 0;
+                    int lb = (int)Math.Round(lenB);
+                    string aVal = "";
+                    string bVal = "";
+                    string processed = "";
+                    foreach (char p in pattern)
+                    {
+                        processed += p;
+                        Console.WriteLine($"Processed: {processed}, now processing:{p}");
+                        if (p == 'a')
+                        {
+                            string subs = value.Substring(position, la);
+                            if (aVal.Length == 0) { aVal = subs; }
+                            else if (aVal != subs) { res = false; break; }
+                            position += la;
+                        }
+                        else // p == 'b'
+                        {
+                            string subs = value.Substring(position, lb);
+                            if (bVal.Length == 0) { bVal = subs; }
+                            else if (bVal != subs) { res = false; break; }
+                            position += lb;
+                        }
+                    }
+                    if (res && aVal != bVal) { return res; }
+                }
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Solution s = new Solution();
+            string pattern = "bbbaa";
+            string value = "xxxxxxy";
+            bool b = s.PatternMatching(pattern, value);
+            Console.WriteLine(b.ToString());
+        }
+    }
+}
--- LeetCode1028/Program.cs
+++ LeetCode1028/Program.cs
@@ -19,10 +19,10 @@ namespace LeetCode1028
         {
             int firstDash = 0;
             while (firstDash < S.Length && S[firstDash] != '-') { firstDash++; }
-            TreeNode root = new TreeNode(int.Parse(S.Substring(0,firstDash).ToString()));
+            TreeNode root = new TreeNode(int.Parse(S.Substring(0, firstDash).ToString()));
             int dashCount = 0;
             StringBuilder sb = new StringBuilder();
-            List<TreeNode> tnList = new List<TreeNode>{ root };
+            List<TreeNode> tnList = new List<TreeNode> { root };
 
             for (int ind = firstDash; ind < S.Length; ++ind)
             {
@@ -34,16 +34,16 @@ namespace LeetCode1028
                     }
                     else
                     {
-                        if (ind == S.Length -1) { sb.Append(S[ind]); }
+                        if (ind == S.Length - 1) { sb.Append(S[ind]); }
                         int val = int.Parse(sb.ToString());
                         sb.Clear();
 
                         try { tnList[dashCount] = new TreeNode(val); }
                         catch (ArgumentOutOfRangeException) { tnList.Add(new TreeNode(val)); }
 
-                        if (tnList[dashCount - 1].left == null) 
+                        if (tnList[dashCount - 1].left == null)
                         { tnList[dashCount - 1].left = tnList[dashCount]; }
-                        else 
+                        else
                         { tnList[dashCount - 1].right = tnList[dashCount]; }
 
                         dashCount = 1;
@@ -58,12 +58,41 @@ namespace LeetCode1028
             return root;
         }
     }
+
+    public class Solution2
+    {
+        public TreeNode RecoverFromPreorder(string S)
+        {
+            MatchCollection mcNumbers = Regex.Matches(S, @"\d+");
+            MatchCollection mcDashes = Regex.Matches(S, @"\-+");
+
+            TreeNode root = new TreeNode(int.Parse(mcNumbers[0].Value));
+            List<TreeNode> tnList = new List<TreeNode> { root };
+            for (int numInd = 1; numInd < mcNumbers.Count; ++numInd)
+            {
+                int dashCount = mcDashes[numInd - 1].Length;
+                int numberVal = int.Parse(mcNumbers[numInd].Value);
+
+                if (tnList.Count <= dashCount)
+                { tnList.Add(new TreeNode(numberVal)); }
+                else
+                { tnList[dashCount] = new TreeNode(numberVal); }
+
+                if (tnList[dashCount - 1].left == null)
+                { tnList[dashCount - 1].left = tnList[dashCount]; }
+                else
+                { tnList[dashCount - 1].right = tnList[dashCount]; }
+            }
+            return root;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             string S = "1-2--3--4-5--6--7";
-            Solution s = new Solution();
+            Solution2 s = new Solution2();
             s.RecoverFromPreorder(S);
         }
     }
