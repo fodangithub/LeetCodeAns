a155c712735d62723c965cd46e63ba43749cad5d
Fodan Deng <fodan.deng@gmail.com>
2020-07-22 16:08:23 +0800

    LC42(unfinished) and LC443

---
+++ LeetCode42/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        /// <summary>
+        /// //// NOT YET FINISHED.
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public int Trap(int[] height)
+        {
+            // (index, val)
+            List<(int,int)> localMax = new List<(int, int)>();
+
+            if (height[0] > height[1]) { localMax.Add((0, height[0])); }
+
+            for(int ind = 1; ind < height.Length - 1; ++ind)
+            {
+                if(height[ind] > height[ind-1] && height[ind] > height[ind+1])
+                {
+                    localMax.Add((ind, height[ind]));
+                }
+            }
+
+            if (height[^2] < height[^1]) { localMax.Add((height.Length - 1, height[^1])); }
+
+            int res = 0;
+            for (int ind = 0; ind < localMax.Count - 1; ++ind)
+            {
+                
+            }
+        }
+    }
+}
---
+++ LeetCode443/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode443
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Solution s = new Solution();
+            Console.WriteLine(s.Compress(new char[] { 'a', 'a', 'a', 'b', 'b' }).ToString());
+        }
+    }
+
+    public class Solution
+    {
+        public int Compress(char[] chars)
+        {
+            if (chars.Length < 1) { return 0; }
+            else if (chars.Length == 1) { return 1; }
+            char lastChar = chars[0];
+            int count = 0;
+            int charCounter = 0;
+            int arrayIndexer = 0;
+            for (int index = 0; index < chars.Length; ++index)
+            {
+                char c = chars[index];
+
+                if (c == lastChar)
+                {
+                    ++charCounter;
+                }
+                else
+                {
+                    chars[arrayIndexer++] = lastChar;
+                    if (charCounter > 1)
+                    {
+                        count += 1 + charCounter.ToString().Length;
+                        foreach (char cc in charCounter.ToString()) { chars[arrayIndexer++] = cc; }
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+                    charCounter = 1;
+                    lastChar = c;
+                }
+
+                if (index == chars.Length - 1)
+                {
+                    chars[arrayIndexer++] = lastChar;
+                    if (charCounter > 1)
+                    {
+                        count += 1 + charCounter.ToString().Length;
+                        foreach (char cc in charCounter.ToString()) { chars[arrayIndexer++] = cc; }
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
