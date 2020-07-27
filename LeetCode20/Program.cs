603f526aaab029e659d5adcc2cdf74a276724ff8
Fodan Deng <fodan.deng@gmail.com>
2020-07-27 15:16:17 +0800

    LC20

---
+++ LeetCode20/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            string str = "([)]";
+            Console.WriteLine($"{s.IsValid(str)}");
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            Dictionary<char, char> validation = new Dictionary<char, char> { [']'] = '[', ['}'] = '{', [')'] = '(' };
+            Stack<char> charStack = new Stack<char>();
+            foreach(char c in s)
+            {
+                if(validation.ContainsKey(c))
+                {
+                    if (charStack.Count < 1 || charStack.Pop() != validation[c])
+                        return false;
+                }
+                else
+                {
+                    charStack.Push(c);
+                }
+            }
+            return charStack.Count < 1;
+        }
+    }
+}
